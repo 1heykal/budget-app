7227e52382e0a26417e1a0e3a71a6bdcbb1a1f66
Osama Heykal <oneheykal@gmail.com>
2024-01-21 14:55:49 +0200

    Initial Commit

---
+++ BudgetApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BudgetApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
